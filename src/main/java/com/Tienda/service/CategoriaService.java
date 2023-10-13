fbb667098ca4b5245a169fcc7a00418ed3f1fb14
luis- <luis-@PC-LUTERMING>
2023-10-13 13:50:29 -0600

    Semana 5

---
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -0,0 +1,8 @@
+package com.Tienda.service;
+
+import com.Tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    public List<Categoria> getCategorias(boolean activo);
+}
